1bb8addfece9b206d39cc81ab2a76d9c0bc5f7a7
Yami1601 <yamini.kumari1620@gmail.com>
2022-03-12 18:54:54 +0000

    edit

--- src/test/java/com/expleo/challengingdom/automation/pages/ChallengingDomPage.java
+++ src/test/java/com/expleo/challengingdom/automation/pages/ChallengingDomPage.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class ChallengingDomPage {
 
 	WebDriver driver;
